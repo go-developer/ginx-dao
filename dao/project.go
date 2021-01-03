88a881ce501029a8ec7fbfd90c4794d6d601e953
张德满 <go_developer@163.com>
2021-01-03 23:42:26 +0800

    增加根据flag获取项目详情

--- dao/project.go
+++ dao/project.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"github.com/go-developer/ginx-dao/define"
 	godb "github.com/go-developer/gorm-mysql"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -39,7 +40,26 @@ func (pd *projectDao) GetProjectList(dbClient *godb.DBClient, optionList ...SetS
 		list []*define.Project
 	)
 	if err = pd.GetDataList(dbClient, define.DBTableProject, &list, optionList...); nil != err {
-		return make([]*define.Project, 0), err
+		return make([]*define.Project, 0), errors.Wrap(err, "获取项目列表失败")
 	}
 	return list, nil
+}
+
+// GetProjectDetailByFlag 根据flag获取项目详情
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 11:32 下午 2021/1/3
+func (pd *projectDao) GetProjectDetailByFlag(dbClient *godb.DBClient, flag string) (*define.Project, error) {
+	var (
+		err    error
+		result define.Project
+	)
+	if err = dbClient.GormDB.Table(define.DBTableProject).Where("flag = ?", flag).First(&result).Error; nil != err {
+		return nil, errors.Wrap(err, "根据flag查询项目详情失败")
+	}
+	if result.ID == 0 {
+		return nil, nil
+	}
+	return &result, nil
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/go-developer/gorm-mysql v0.0.0-20200802121817-f502e27ee318
 	github.com/jinzhu/gorm v1.9.14
+	github.com/pkg/errors v0.8.1
 )
